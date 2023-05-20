9c05b903d73105a78004faf33b3e680ab8d47236
Thompsonclass <bbb7219@naver.com>
2023-05-20 21:00:54 +0900

    로그인 및 회원가입 컴포넌트

--- Component_Authentication/JoinApp.jsx
+++ Component_Authentication/JoinApp.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState} from 'react';
 
-const JoinApp = () => {
+const JoinApp = (props) => {
+  const [inputIdValue, setInputIdValue] = useState(''); //ID값 저장
+  const [inputPassValue, setInputPassValue] = useState(''); //PW값 저장
+
+  const mainJoin = () => {
+
+  };
+
   return (
     <div>
-      
+      <div>
+        <label>아이디 : </label>
+        <input
+          type="text"
+          value={inputIdValue}
+          onChange={(e) => {
+            setInputIdValue(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <label>비밀번호 : </label>
+        <input
+          type="password"
+          value={inputPassValue}
+          onChange={(e) => {
+            setInputPassValue(e.target.value);
+          }}
+        />
+      </div>
+      <button onClick={mainJoin}>회원가입</button>
     </div>
-  )
-}
+  );
+};
 
-export default JoinApp
+export default JoinApp;
--- Component_Authentication/LoginApp.jsx
+++ Component_Authentication/LoginApp.jsx
@@ -2,9 +2,42 @@ import React, { useState } from 'react';
 import JoinApp from './JoinApp';
 
 const LoginApp = () => {
+  const [inputIdValue, setInputIdValue] = useState(''); //ID값 저장
+  const [inputPassValue, setInputPassValue] = useState(''); //PW값 저장
+  
+  const mainLogin = () => {
+
+  }
+
+  const JoinClick = () => {
+
+  }
   return (
     <div>
-      
+        <div>
+          <div>
+            <label>아이디 : </label>
+            <input
+              type="text"
+              value={inputIdValue}
+              onChange={(e) => {
+                setInputIdValue(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label>비밀번호 : </label>
+            <input
+              type="password"
+              value={inputPassValue}
+              onChange={(e) => {
+                setInputPassValue(e.target.value);
+              }}
+            />
+          </div>
+          <button onClick={mainLogin}>로그인</button>
+          <button onClick={JoinClick}>회원가입</button>
+        </div>
     </div>
   )
 }
