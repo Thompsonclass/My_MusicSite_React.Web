4eff09030b3e8d5bed60f720cb7808c224a4b8f8
Thompsonclass <bbb7219@naver.com>
2023-05-20 22:39:57 +0900

    로그인 및 회원정보 컴포넌트

--- Component_Authentication/JoinApp.jsx
+++ Component_Authentication/JoinApp.jsx
@@ -1,16 +1,21 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 
 const JoinApp = (props) => {
   const [inputIdValue, setInputIdValue] = useState(''); //ID값 저장
   const [inputPassValue, setInputPassValue] = useState(''); //PW값 저장
 
   const mainJoin = () => {
-    if(inputIdValue === ''){alert("설정할 아이디를 입력하세요.")} 
-    else if(inputPassValue === ''){alert("설정할 비밀번호를 입력하세요.")}
+    if (inputIdValue === '') {
+      alert('설정할 아이디를 입력하세요.');
+    } else if (inputPassValue === '') {
+      alert('설정할 비밀번호를 입력하세요.');
+    }
+
     const data = {
       inputIdValue,
       inputPassValue,
     };
+
     props.onJoin(data); //설정한 ID와 PW를 부모 컴포넌트로 전달, onJoin(data)함수 호출
   };
 
@@ -21,7 +26,7 @@ const JoinApp = (props) => {
         <input
           type="text"
           value={inputIdValue}
-          placeholder='설정할 아이디를 입력하세요.'
+          placeholder="설정할 아이디를 입력하세요."
           onChange={(e) => {
             setInputIdValue(e.target.value);
           }}
@@ -32,7 +37,7 @@ const JoinApp = (props) => {
         <input
           type="password"
           value={inputPassValue}
-          placeholder='설정할 비밀번호를 입력하세요.'
+          placeholder="설정할 비밀번호를 입력하세요."
           onChange={(e) => {
             setInputPassValue(e.target.value);
           }}
--- Component_Authentication/LoginApp.jsx
+++ Component_Authentication/LoginApp.jsx
@@ -5,25 +5,38 @@ const LoginApp = () => {
   const [inputIdValue, setInputIdValue] = useState(''); //ID값 저장
   const [inputPassValue, setInputPassValue] = useState(''); //PW값 저장
   const [JoinShowValue, setJoinShowValue] = useState(false); //JoinApp component로 이동
-  const [customerData, setcustomerData] = useState('');
+  const [customerData, setcustomerData] = useState([]); //회원 정보(객체) 저장
   
   const mainLogin = () => {
-
-  }
+    if(inputIdValue === '') {
+      return alert("아이디를 입력하세요.")
+    } else if(inputPassValue === '') {
+      return alert("비밀번호를 입력하세요.")
+    }
+    for(let i = 0; i < customerData.length; i++) {
+      const userTest = customerData[i] //customerData 배열의 요소를 userTest 변수에 할당
+      if (userTest.inputIdValue === inputIdValue && userTest.inputPassValue === inputPassValue) {
+        alert("성공적으로 로그인 되었습니다.");
+      } else {
+        alert("잘못 입력하였습니다.");
+      }
+    }
+  } 
 
   const onJoin = (data) => {
     setcustomerData([...customerData, data]);
-    inputIdValue('');
-    inputPassValue('');
-  }
+    setInputIdValue(''); //LoginApp ID값 빈셀
+    setInputPassValue(''); //LoginAPp PW값 빈셀
+    setJoinShowValue(false); //다시 LoginApp 컴포넌트 Show
+  };
 
   const JoinClick = () => {
     setJoinShowValue(true);
-  }
+  };
 
   return (
     <div>
-      {!JoinShowValue?( //JoinShowValue === true
+      {!JoinShowValue ? ( //JoinShowValue === true
         <div>
           <div>
             <label>아이디 : </label>
@@ -50,7 +63,9 @@ const LoginApp = () => {
           <button onClick={mainLogin}>로그인</button>
           <button onClick={JoinClick}>회원가입</button>
       </div>
-      ) : (<JoinApp onJoin = {onJoin}/>)}
+      ) : (
+      <JoinApp onJoin = {onJoin}/>
+      )}
     </div>
   )
 }
