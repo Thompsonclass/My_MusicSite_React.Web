506e1c3509903db16274829be7a2d609bfed177d
Thompsonclass <bbb7219@naver.com>
2023-05-21 22:36:45 +0900

    Main

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react'
 import LoginApp from './Component_Authentication/LoginApp';
+import AppSongPlayList from './Component_Main/AppSongPlayList';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <LoginApp />
+      <AppSongPlayList />
     </div>
   );
 }
---
+++ Component_Main/AppSongPlayList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppSongPlayList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppSongPlayList
