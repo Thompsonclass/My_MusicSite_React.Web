736fb67fe6948deadc6756da71572aeba7563b57
Thompsonclass <bbb7219@naver.com>
2023-05-22 17:22:58 +0900

    AppSongPlayList

--- Component_Main/AppSongPlayList.jsx
+++ Component_Main/AppSongPlayList.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 function AppSongPlayList() {
+  const backStyleFloor = {
+    background: "pink",
+    height: "100px",
+  };
+
+  const leftDivStyle = {
+    display: "flex",
+    flexDirection: "column", // 수직 방향으로 정렬
+    width: "20%",
+    height : "100px"
+  };
+
+  const playlistStyle = {
+    display: "flex",
+    border: "1px solid black",
+  };
+
   return (
     <div>
-      
+      <div style={backStyleFloor}>각 아이콘 지정</div>
+      <div style={leftDivStyle}>
+        <div style={playlistStyle}>
+          <div>
+            <h3>제목</h3>
+            <p>노래</p>
+          </div>
+          <div>
+            <button>즐겨찾기 추가</button>
+          </div>
+        </div>
+        <div style={playlistStyle}>
+          <div>
+            <h3>제목</h3>
+            <p>노래</p>
+          </div>
+          <div>
+            <button>즐겨찾기 추가</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AppSongPlayList
+export default AppSongPlayList;
