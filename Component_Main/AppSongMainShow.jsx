534afc30505b5d0bf38b6477932dfc3b2cc5ba3f
Thompsonclass <bbb7219@naver.com>
2023-07-11 01:11:17 +0900

    ComoponentMain

--- Component_Main/AppSongMainShow.jsx
+++ Component_Main/AppSongMainShow.jsx
@@ -2,10 +2,49 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AppSongFavoriteList from './AppSongFavoriteList';
 
-const AppSongMainShow = () => {
+import { AppProps } from "next/App";
+import Script from "next/script";
+
+const AppSongMainShow = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  
   useEffect(() => {
     document.title = '홈페이지';
   });
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new window.Spotify.Player({
+        name: "Web Playback SDK",
+        getOAuthToken: (cb: any) => {
+          // Run code to get a fresh access token
+          cb(token?.replace(/\"/g, "")); // 이 token은 로그인 시에 백엔드에서 받아 온 access token이다.
+        },
+        volume: 0.5,
+      });
+
+      player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
+
+      player.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
+      player.addListener("player_state_changed", (state) => {
+        if (!state) {
+          return;
+        }
+        console.log("state changed", state);
+      });
+
+      player.connect();
+    };
+  }, [token]);
 
   return (
     <>
@@ -25,6 +64,8 @@ const AppSongMainShow = () => {
       </Router>
       <h1>AppSongPlayList</h1>
       <h1>AppSongPlayer</h1>
+      <Script src="https://sdk.scdn.co/spotify-player.js" />
+      <Component {...pageProps} />
     </>
   );
 }
