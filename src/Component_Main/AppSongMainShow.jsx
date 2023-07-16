c75eeea699a2653a5167963d450ffb4337972c5d
Thompsonclass <bbb7219@naver.com>
2023-07-17 03:57:19 +0900

    MainComponent

--- src/Component_Main/AppSongMainShow.jsx
+++ src/Component_Main/AppSongMainShow.jsx
@@ -7,11 +7,11 @@ import AudioPlayer from 'react-audio-player';
 import SiderList from './SiderList';
 
 const AppSongMainShow = () => {
-  const [Playing, setPlaying] = useState(false); // 재생
+  const [Playing, setPlaying] = useState(false); //재생,일시정지 버튼
   const [volume, setVolume] = useState(50);
   const audioRef = useRef(null);
 
-  const handlePlayPause = () => { //재생
+  const handlePlayPause = () => { //재생,일시정지 버튼
     setPlaying(!Playing);
   };
 
@@ -31,34 +31,37 @@ const AppSongMainShow = () => {
       <Routes>
         <Route path="/page1" element={<AppSongFavoriteList />} />
       </Routes>
-
-      <div className="songPlayer">
-        <span className = "IconButton">
-        <IconButton onClick={handlePlayPause}>
-          {Playing ? <Pause /> : <PlayArrow />} 
-        </IconButton>
-        </span>
-        <span>
-        <Slider
-          value={volume}
-          onChange={handleVolumeChange}
-          min={0}
-          max={100}
-          aria-labelledby="volume_slider"
-          className="volume-slider"
-          style={{ width: '150px' }}
+      
+      <div>
+        <div className="songPlayer">
+          <div className='songMainTitle'>
+            <h3 className="songTitle">[ Jazz_Happy ]</h3>
+            <img src="http://localhost:3000/songImages/jazzhappy.jpg" id="img" alt="Jazz Happy" />
+          </div>
+          <div className='songControl'>
+            <IconButton onClick={handlePlayPause}>
+              {Playing ? <Pause /> : <PlayArrow />} 
+            </IconButton>
+            <Slider
+              value={volume}
+              onChange={handleVolumeChange}
+              min={0}
+              max={100}
+              aria-labelledby="volume_slider"
+              className="volume-slider"
+              style={{ width: '150px' }}
         />
-        </span>
-          <VolumeUp className="volumeIcon" />
+            <VolumeUp className="volumeIcon" />
+          </div> 
           <AudioPlayer
             ref={audioRef}
-            src="http://localhost:3000/songs/jazz-happy"
+            src="http://localhost:3000/songs/jazz-happy-110855.mp3"
             volume={volume / 100}
             autoPlay={false}
             controls={false}
         />
+        </div>
       </div>
-
     </Router>
   );
 }
