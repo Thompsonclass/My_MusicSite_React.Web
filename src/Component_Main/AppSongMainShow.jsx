4f287179f3fbfc1a642a49c5a849a0b056d8f0e0
Thompsonclass <bbb7219@naver.com>
2023-07-21 00:02:54 +0900

    MainComponent

--- src/Component_Main/AppSongMainShow.jsx
+++ src/Component_Main/AppSongMainShow.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { IconButton, Slider } from '@material-ui/core';
-import { PlayArrow, Pause, VolumeUp } from '@material-ui/icons';
 import AppSongFavoriteList from './AppSongFavoriteList';
 import AudioPlayer from 'react-audio-player';
 import SiderList from './SiderList';
 
+import AppSongPlayer from './AppSongPlayer';
+
 const AppSongMainShow = () => {
-  const [volume, setVolume] = useState(50);
+  const [Playing, setPlaying] = useState(false);
   const audioRef = useRef(null);
 
+  const PlayingButton = () => {
+
+  }
+  
   return (
     <Router>
       <div className='SliderList'>
@@ -20,22 +24,27 @@ const AppSongMainShow = () => {
       <Routes>
         <Route path="/page1" element={<AppSongFavoriteList />} />
       </Routes>
-
-      <div>
-        <div className="songPlayer">
-          <div className='songMainTitle'>
-            <h3 className="songTitle">[ catch-it ]</h3>
-            <img src="/songImages/jazzhappy.jpg" id="img" alt="catch-it" />
+      <div className='AppSongMainPlayer'>
+        <div>
+          <div className="songPlayer">
+            <div className='songMainTitle'>
+              <h3 className="songTitle">[ catch-it ]</h3>
+              <img src="/songImages/jazzhappy.jpg" id="img" alt="catch-it" />
+            </div>
+            <div className='songControl'>
+              <button onClick={PlayingButton}>{Playing ? 'Pause' : 'Play'}</button>
+            </div>
+            <AudioPlayer
+              ref={audioRef}
+              src="/songs/catch-it-117676.mp3"
+              volume={50 / 100}
+              autoPlay={false}
+              controls={true}
+            />
           </div>
-          <div className='songControl'>
-          </div>
-          <AudioPlayer
-            ref={audioRef}
-            src="/songs/catch-it-117676.mp3"
-            volume={volume / 100}
-            autoPlay={false}
-            controls={true}
-          />
+        </div>
+        <div className='AppSongPlayer'>
+          <AppSongPlayer Playing={Playing} />
         </div>
       </div>
     </Router>
--- src/Component_Main/AppSongPlayer.jsx
+++ src/Component_Main/AppSongPlayer.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
 
-const AppSongPlayer = () => {
+const AppSongPlayer = (props) => {
+  const songPlayerStyle = {
+    backgroundColor: "rgb(226, 235, 243)",
+    width: "1380px",
+    height: "800px",
+    border: "solid 2px lightblue",
+    position: "fixed",
+  };
+
+  const cdStyle = {
+    width: "200px",
+    height: "200px",
+    borderRadius: "50%",
+    background: "lightgray",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%) rotate(0deg)", // 회전 시작 각도
+    animation: "spin 2s linear infinite", // 애니메이션
+    overflow: "hidden",
+  };
+
+  const imageStyle = {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover", // 이미지 비율 
+  };
+
   return (
     <>
-        
+      <div className='AppSongPlayer' style={songPlayerStyle}>
+        <div style={cdStyle}>
+          <img
+            src="/songImages/jazzhappy.jpg"
+            alt="CD"
+            style={imageStyle}
+          />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AppSongPlayer
+export default AppSongPlayer;
