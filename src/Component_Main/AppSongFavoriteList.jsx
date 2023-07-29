46ed297d7e093a444f6e720954c86b82bd62c543
Thompsonclass <bbb7219@naver.com>
2023-07-29 19:27:51 +0900

    MainComponent

--- src/Component_Main/AppSongFavoriteList.jsx
+++ src/Component_Main/AppSongFavoriteList.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function AppSongFavoriteList() {
+  const [favoriteSongs, setFavoriteSongs] = useState([]);
+
   useEffect(() => {
     document.title = '즐겨찾기 목록';
   }, []);
 
   return (
     <>
-     <h1>다음은 원하는 노래 리스트 컴포넌트입니다.</h1>
+      <h1>다음은 원하는 노래 리스트 컴포넌트입니다.</h1>
+      <ul>
+        {favoriteSongs.map((song, _) => (
+          <li>{song}</li>
+        ))}
+      </ul>
     </>
   );
 }
