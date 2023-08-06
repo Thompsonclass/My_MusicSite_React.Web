dd73cd8b1946c7459ca779515e8a5c4a9b0b913c
Thompsonclass <bbb7219@naver.com>
2023-08-06 23:54:33 +0900

    MainComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Routes, Route, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginApp from './Component_Authentication/LoginApp';
 import AppSongMainHome from './Component_Main/AppSongMainHome';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginApp />} />
-        <Route path="/Music_player_main" element={<AppSongMainHome />} />
-      </Routes>
+      <div>
+        <Route exact path="/" component={<LoginApp />} />
+        <Route path="/main" component={<AppSongMainHome />} />
+      </div>
     </Router>
   );
 };
