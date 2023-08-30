07daec008883639125315d03ac86476b7f61292d
Thompsonclass <bbb7219@naver.com>
2023-08-30 16:12:37 +0900

    MainComponent

--- src/Component_Main/Component_Music_Spectrum/MusicSpectrumPlay.jsx
+++ src/Component_Main/Component_Music_Spectrum/MusicSpectrumPlay.jsx
@@ -3,7 +3,7 @@ import ReactWaves from "@dschoon/react-waves";
 import { useGlobalStateContext } from '../../Component_GlobalState/GlobalStateContent';
 
 function MusicSpectrumPlay({ song }) {
-  const { trackIndex, playing } = useGlobalStateContext();
+  const { trackIndex, playing } = useGlobalStateContext(); //
 
   return (
     <div>
@@ -11,12 +11,11 @@ function MusicSpectrumPlay({ song }) {
         <ReactWaves
           audioFile={song.musicSrc} 
           options={{
-            backend: 'MediaElement',
-            normalize: true,
-            cursorWidth: 0,
-            mediaType: 'audio',
-            hideScrollbar: true,
-            responsive: true,
+            barWidth: 0, // 웨이브의 바(진행 상태 바) 너비를 설정합니다.
+            cursorWidth: 0, // 커서의 너비
+            height: 70, // 오디오 시각화의 높이
+            hideScrollbar: true, // 스크롤바 숨김 여부
+            responsive: true, // 반응형 디자인 사용 여부
           }}
           zoom={1}
           playing={playing}
