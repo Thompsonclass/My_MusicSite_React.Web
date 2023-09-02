8a7448cd295c7fe82e28bdcd06701f1e1a4ee426
Thompsonclass <bbb7219@naver.com>
2023-09-02 23:47:27 +0900

    MainComponent

--- src/Component_Main/Component_PlayList_SongKind/Component_PlayList_Jazz/AppSongJazzContent.jsx
+++ src/Component_Main/Component_PlayList_SongKind/Component_PlayList_Jazz/AppSongJazzContent.jsx
@@ -2,7 +2,35 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AppSongMainTitle from '../../../Component_Title/AppSongMainTitle'
 import { StyledMainHomeWrapper } from '../../../Component_MainHomeWrapper/Read.styled';
+import styled from 'styled-components';
 
+const JazzParentContainer = styled.div`
+  border: 2px solid black;
+  margin-left: 2em;
+  width: 112em;
+  display: flex;
+  felx-direction: row;
+`
+const Titleh2 = styled.div`
+  border: 2px solid black;
+  border-bottom: none;
+  width: 10em;
+  height: 3em;
+  margin-top: 3em;
+  margin-left: 2em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const ListsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 2px solid green;
+  width: 10em;
+  margin: 10px;
+  font-weight: bold;
+  font-size: 20px;
+`
 
 function AppSongJazzContent() {
   const [audioAllData, setAudioAllData] = useState([]); // 모든 노래 리스트
@@ -35,22 +63,20 @@ function AppSongJazzContent() {
 
   return (
     <StyledMainHomeWrapper>
-      <AppSongMainTitle />
+          <AppSongMainTitle />
+        <Titleh2>
+          <h2>◆ Jazz 노래</h2>
+        </Titleh2>
+      <JazzParentContainer>
         {audioAllLists.map((data, index) => (
           <div key={index}>
-            <p>{data.name}</p><p>{data.singer}</p>
-            <button onClick={() => AddSongList(index)}>추가</button>
+            <ListsContainer>
+              <p>{data.name}</p><p>{data.singer}</p> {/* 노래 제목, 가수 */}
+              <button onClick={() => AddSongList(index)}>추가</button>
+            </ListsContainer>
           </div>
         ))}
-      {/* 선택된 노래 목록 렌더링 */}
-      <div>
-        <h2>선택된 노래 목록</h2>
-        {selectedSongs.map((song, index) => (
-          <div key={index}>
-            <p>{song.name}</p><p>{song.singer}</p>
-          </div>
-        ))}
-      </div>
+      </JazzParentContainer>
     </StyledMainHomeWrapper>
   );
 }
