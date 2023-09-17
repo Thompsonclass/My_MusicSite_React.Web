c224e8342ec61af1cd55bbebc659bfe552f9a09c
Thompsonclass <bbb7219@naver.com>
2023-09-17 23:58:16 +0900

    MainComponent

---
+++ src/Component_Main/Component_PlayList_SongKind/Component_PlayList_classic/AppSongClassicBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppSongClassicBtn() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppSongClassicBtn
---
+++ src/Component_Main/Component_PlayList_SongKind/Component_PlayList_classic/AppSongClassicContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppSongClassicContent() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppSongClassicContent
